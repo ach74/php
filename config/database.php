7e5b60855de253c4ac6319e47e1ac0f1457ea5db
ach74 <achraf074@gmail.com>
2018-10-18 18:18:03 +0200

    ach

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+// used to connect to the database
+$host = "localhost";
+$db_name = "php_beginner_crud_level_1";
+$username = "root";
+$password = "";
+  
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+
+    echo "Ha funcionado correctamente";
+}
+
+  
+// show error
+catch(PDOException $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
+?>
